765c8bc22c0c3b301f069bfc4d78b520ceabb912
adonisestolaza <72994994+adonisestolaza@users.noreply.github.com>
2021-06-18 10:47:22 -0400

    microservicios-commons

---
+++ microservicios-SpringCloud/commons-microservicios/src/main/java/com/process/workflow/app/commons/service/CommonService.java
@@ -0,0 +1,20 @@
+package com.process.workflow.app.commons.service;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+
+public interface CommonService<E> {
+	
+	public Page<E> findAll(Pageable pageable);
+	
+	public Iterable<E> findAll();
+	
+	public Optional<E> findById(Long id);
+	
+	public E save(E entity);
+	
+	public void deleteById(Long id);
+}
